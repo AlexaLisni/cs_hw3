999d1cff6653c2835afabb8dafa9677aead9e1bf
Alexandr <sashylay0@gmail.com>
2022-08-15 10:50:32 +0300

    finish task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//  является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine("Напишите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99999 || num < 10000)
+{
+    Console.WriteLine("неверное число ");
+}
+else if (num / 10000 == num % 10 && (num / 1000) % 10 == (num % 100) / 10)
+{
+    Console.WriteLine($"{num} -> ДА");
+}
+else
+{
+    Console.WriteLine($"{num} -> НЕТ");
+}
+
+
