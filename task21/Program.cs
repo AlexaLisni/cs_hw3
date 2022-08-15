b38cda4c064bc4a06de09aaaaed35f7f693b998b
Alexandr <sashylay0@gmail.com>
2022-08-15 11:03:59 +0300

    finish task21

--- task21/Program.cs
+++ task21/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
+//  в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Введите кординаты точки A");
+Console.Write("X:");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y:");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z:");
+int zA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кординаты точки B");
+Console.Write("X:");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y:");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z:");
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double Line(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+}
+double result = Line(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"расстояние между точками в 3D пространстве -> {Math.Round(result, 2)}");
